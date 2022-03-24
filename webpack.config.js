020e01b1b750c4e9848c65c2cd3d400525f48ea3
Nientai Ho <hntddt123@gmail.com>
2022-03-23 21:53:57 -0500

    Added webpack babel-loader, ts-loader, file-loader and configs

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const prod = process.env.NODE_ENV === 'production';
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  mode: prod ? 'production' : 'development',
+  entry: './src/index.tsx',
+  output: {
+    path: `${__dirname}/dist/`,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        resolve: {
+          extensions: ['.ts', '.tsx', '.js', '.json'],
+        },
+        use: 'ts-loader',
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.(jpg|jpeg|png|gif|mp3|svg)$/,
+        use: ['file-loader'],
+      },
+    ]
+  },
+  devtool: prod ? undefined : 'source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: '/public/index.html',
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+};
