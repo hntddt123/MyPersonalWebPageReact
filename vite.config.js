b42f8dc008d625c6f7347fa644b36552b91dc35f
Nientai Ho <hntddt123@gmail.com>
2025-06-28 01:43:26 +0800

    Updated base and fixed dist folder for deployment

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/',
+  base: '/MyPersonalWebPageReact/',
   plugins: [react()],
   preview: {
     port: 3000,
